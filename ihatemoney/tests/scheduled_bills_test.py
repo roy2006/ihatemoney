0de767e1636a3c7c947dbe4e7c222637eb1d7134
Roy Emek <roy.emek@gmail.com>
2022-12-08 19:15:37 +0200

    added a couple of tests for scheduled bills

--- ihatemoney/tests/scheduled_bills_test.py
+++ ihatemoney/tests/scheduled_bills_test.py
@@ -16,13 +16,14 @@ from wtforms.fields import SelectField
  
 from unittest.mock import patch
 
-def make_field():
-    recurring_schedule = SelectField(label="Repeat every", choices=[(1, "test-value")], coerce=int, default = 0)
+def allow_1_sec_schedule():
+    choices = [(1, "1-sec"), (3600, "1 Hour")]
+    recurring_schedule = SelectField(label="Repeat every", choices=choices, coerce=int, default = 0)
     return recurring_schedule
 
 class ScheduledBillsTestCase(IhatemoneyTestCase):
 
-    @patch("ihatemoney.forms.BillForm.recurring_schedule", new_callable=make_field)
+    @patch("ihatemoney.forms.BillForm.recurring_schedule", new_callable=allow_1_sec_schedule)
     def test_recurring_bill(self, recurring_schedule_mock):
         self.post_project("raclette")
         # add two participants
@@ -47,14 +48,86 @@ class ScheduledBillsTestCase(IhatemoneyTestCase):
         p = self.get_project("raclette")
         assert len(p.get_bills().all()) == 1
 
-        time.sleep(2) 
+        time.sleep(1.5) 
         assert len(p.get_bills().all()) > 1
 
-        # bill_id = p.get_bills().first().id
-        # self.client.post(f"/raclette/duplicate/{bill_id}", data={})
+    @patch("ihatemoney.forms.BillForm.recurring_schedule", new_callable=allow_1_sec_schedule)
+    def test_edit_bill_schedule(self, recurring_schedule_mock):
+        self.post_project("raclette")
+        # add two participants
+        self.client.post("/raclette/members/add", data={"name": "zorglub"})
+        self.client.post("/raclette/members/add", data={"name": "tata"})
 
-        # all_bills = p.get_bills().all()
-        # assert len(all_bills) == 2
+        members_ids = [m.id for m in self.get_project("raclette").members]
 
-        # assert "duplicate" in all_bills[0].what
-        # assert all_bills[0].amount == all_bills[1].amount
+        # test balance
+        self.client.post(
+            "/raclette/add",
+            data={
+                "date": "2011-08-10",
+                "what": "fromage à raclette",
+                "payer": members_ids[0],
+                "payed_for": members_ids,
+                "amount": "100",
+                "recurring_schedule": 1
+            },
+        )
+
+        p = self.get_project("raclette")
+        assert len(p.get_bills().all()) == 1
+
+        bill_id = p.get_bills().first().id
+
+        resp = self.client.post(
+            f"/raclette/edit/{bill_id}",
+            data={
+                "date": "2011-08-10",
+                "what": "fromage à raclette",
+                "payer": members_ids[0],
+                "payed_for": members_ids,
+                "amount": "100",
+                "recurring_schedule": 3600
+            },
+        )
+        
+        # No new bills should be created
+        time.sleep(1.5) 
+        assert len(p.get_bills().all()) == 1
+
+    @patch("ihatemoney.forms.BillForm.recurring_schedule", new_callable=allow_1_sec_schedule)
+    def test_delete_bill_schedule(self, recurring_schedule_mock):
+        self.post_project("raclette")
+        # add two participants
+        self.client.post("/raclette/members/add", data={"name": "zorglub"})
+        self.client.post("/raclette/members/add", data={"name": "tata"})
+
+        members_ids = [m.id for m in self.get_project("raclette").members]
+
+        # test balance
+        self.client.post(
+            "/raclette/add",
+            data={
+                "date": "2011-08-10",
+                "what": "fromage à raclette",
+                "payer": members_ids[0],
+                "payed_for": members_ids,
+                "amount": "100",
+                "recurring_schedule": 1
+            },
+        )
+
+        p = self.get_project("raclette")
+        assert len(p.get_bills().all()) == 1
+
+        bill_id = p.get_bills().first().id
+
+        resp = self.client.post(
+            f"/raclette/delete/{bill_id}",
+            data={ } 
+        )
+        
+        # No new bills should be created
+        time.sleep(1.5) 
+        assert len(p.get_bills().all()) == 0
+
+
